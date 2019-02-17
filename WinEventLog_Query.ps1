6ec6a7da55469ea0ba17e4015e4bb9144507a54c
Romel <43550213+rpc180@users.noreply.github.com>
2019-02-16 22:32:56 -0500

    Events now added to array
    Populates array with the qualified events so they can be viewed by time instead of grouped by log provider

--- WinEventLog_Query.ps1
+++ WinEventLog_Query.ps1
@@ -1,4 +1,4 @@
-﻿$startscript = get-date
+$startscript = get-date
 $shortdate = get-date -format MM-dd-yy
 $systemrunning = hostname
 $runpath = get-location
@@ -8,11 +8,26 @@ Write-output "Starting script at $startscript" | Tee-object -filepath $logfile
 Write-output "log file location: $logfile" | Tee-Object -filepath $logfile -append
 Write-output "This script is executing on $systemrunning" | Tee-Object -filepath $logfile -append
 Write-output "in directory $runpath"
+add-content $logfile "`r"
 
 $targetsystem = read-host -prompt "ENTER target computername as FQDN"
-$scope = read-host -prompt "(Time) based results or (Last) entries"
+$scope = read-host -prompt "(Time) based results or (Latest) entries"
 $verbose = read-host -prompt "Enter verbose mode (True) or (False)"
-$numberofevents = read-host -prompt "Enter maximum events to return"
+$numberofevents = read-host -prompt "Enter maximum events per log to return"
+
+$eventarray = @()
+$logswithentries = get-winevent -computername $targetsystem -listlog * | where-object { $_.recordcount -gt 0 }
+
+Function Add-EventDetails {
+    $eventconvert = [xml]$event.toxml()
+    $timecreated = get-date -date $eventconvert.event.system.timecreated.systemtime
+    $provider = $eventconvert.event.system.provider.Name
+    $Level = $event.LevelDisplayName
+    $EID = $event.Id
+    $msg = $event.message
+    $newobj = [PSCustomObject]@{ 'Timestamp' = $timecreated; 'Log' = $provider; 'Level' = $level; 'EventID' = $EID; 'Message' = $msg }
+    $global:eventarray+=$newobj
+    }
 
 Switch ( $scope )
     {
@@ -21,31 +36,35 @@ Switch ( $scope )
                 $beginrange = read-host -prompt "Enter initial date/time in format '11/2/2018 10:32am'"
                 $endrange = read-host -prompt "Enter end date/time"
                 $starttime = get-date $beginrange
-                $endtime = get-date $endrange
-                $logswithentries = get-winevent -computername $targetsystem -listlog * | where-object { $_.recordcount -gt 0 }
+                $endtime = get-date $endrange 
                 foreach ( $log in $logswithentries ) {
-                    if ( $verbose -eq 'True' ) 
-                        {
-                        get-winevent -computername $targetsystem -logname $log.logname -maxevents $numberofevents | where-object {($_.TimeCreated -ge $starttime -and $_.timecreated -le $endtime)} | ft -auto -wrap | write-output | tee-object $logfile -append                      
+                    $events = get-winevent $log.logname -maxevents $numberofevents | where-object {($_.TimeCreated -ge $starttime -and $_.timecreated -le $endtime)}
+                        foreach ( $event in $events ) {
+                            Add-EventDetails
                         }
-                    elseif ( $verbose -eq 'False' ) 
-                        {            
-                        get-winevent -computername $targetsystem -logname $log.logname -maxevents $numberofevents | where-object {($_.TimeCreated -ge $starttime -and $_.timecreated -le $endtime)} | write-output | tee-object $logfile -append
-                        }
-                    }
-             }
-        Last
+                }
+            }
+        Latest
             {
-                $logswithentries = get-winevent -computername $targetsystem -listlog * | where-object { $_.recordcount -gt 0 }
                 foreach ( $log in $logswithentries ) {
-                    if ( $verbose -eq 'True' )
-                        {
-                        get-winevent -computername $targetsystem -logname $log.logname -maxevents $numberofevents | ft -auto -wrap | write-output | tee-object $logfile -append
+                    $events = get-winevent $log.logname -maxevents $numberofevents
+                        foreach ( $event in $events ) {
+                            Add-EventDetails
                         }
-                    elseif ( $verbose -eq 'False' )
-                        {
-                        get-winevent -computername $targetsystem -logname $log.logname -maxevents $numberofevents | write-output | tee-object $logfile -append
-                        }
-                    }
+                }
+            }
+    }                           
+
+Switch ( $verbose )
+    {
+        True
+            {
+                $eventarray = $eventarray | sort-object -property "Timestamp" -Descending | ft -AutoSize -wrap 
+            }
+        False
+            {
+                $eventarray = $eventarray | sort-object -property "Timestamp" -Descending | ft 
             }
     }
+    
+$eventarray | tee-object $runlog -append
